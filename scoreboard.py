00ac78a20ababa0ff4811c8ecce4e6ea76f32170
Abasar03 <109254642+Abasar03@users.noreply.github.com>
2024-08-04 10:12:08 +0545

    final update

--- scoreboard.py
+++ scoreboard.py
@@ -7,11 +7,12 @@ class Scoreboard(Turtle):
         self.score=0
         with open("data.txt",mode="r") as data:
             value=(data.read())
-            # if value:
-            self.high_score = int(value)
-            # else:
-            #         print("The file is empty. Setting high score to 0.")
-            #         self.high_score = 0
+            if value == "":
+                print("The file is empty. Setting high score to 0.")
+                self.high_score = 0
+            else:
+                self.high_score = int(value)
+
         self.color("white")
         self.penup()
         self.goto(0,268)
@@ -22,17 +23,21 @@ class Scoreboard(Turtle):
         self.clear()
         self.write(f"Score:{self.score} High score:{self.high_score}", align=ALIGNMENT, font=FONT)
 
+
     def reset_score(self):
         if self.score>self.high_score:
-            self.high_score=self.score
+            self.high_score = self.score
             with open("data.txt",mode="w")as data:
                 data.write(f"{self.high_score}")
-
         self.score=0
         self.update_score()
 
     def increase_score(self):
         self.score += 1
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.update_score()
 
     def game_over(self):
